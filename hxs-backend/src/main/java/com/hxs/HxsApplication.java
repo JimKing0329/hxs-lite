3faf81ef260c6a0b1fb8cd5908c50e042e032ca8
wangxin <xxx@163.com>
2026-05-20 16:23:56 +0800

    Initial commit: hxs-lite 重构版本

---
+++ hxs-backend/src/main/java/com/hxs/HxsApplication.java
@@ -0,0 +1,13 @@
+package com.hxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HxsApplication.class, args);
+    }
+
+}
---
+++ hxs-backend/src/test/java/com/hxs/HxsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
